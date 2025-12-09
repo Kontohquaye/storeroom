f36702eae80859dfa64112d489a6c34670c9dec6
Kontohquaye <ekontoh07@gmail.com>
2025-12-09 20:47:30 +0000

    add coupon form

--- components/Store.tsx
+++ components/Store.tsx
@@ -1,10 +1,12 @@
+import { auth } from "@/auth";
 import DisplayStore from "./DisplayStore";
 import { EmptyStore } from "./EmptyStore";
 import { fetchStore } from "@/lib/actions";
 
 const StoreRoom = async () => {
-  const storeList = fetchStore();
-  const stores = await storeList;
+  const session = await auth();
+  const storeList = session?.user?.id && fetchStore();
+  const stores = (await storeList) || [];
   // console.log(stores);
   return (
     <div className="content">
